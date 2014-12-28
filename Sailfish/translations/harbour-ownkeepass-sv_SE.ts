e187854939ce96c709ded1f73a50a1b55a8da16e
Marko Koschak <marko.koschak@tisno.de>
2014-12-29 00:47:28 +0100

    Update formating of .ts files

--- Sailfish/translations/harbour-ownkeepass-sv_SE.ts
+++ Sailfish/translations/harbour-ownkeepass-sv_SE.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="sv_SE" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv_SE">
 <context>
     <name>AboutPage</name>
     <message>
